992f2b9847d01bf3aeefcba7253899969acca95d
Tyler Glen <tylerstallworth15@gmail.com>
2025-12-11 23:47:40 -0500

    alertstyle

--- src/components/AlertBox/AlertBox.tsx
+++ src/components/AlertBox/AlertBox.tsx
@@ -7,9 +7,16 @@ export const AlertBox: React.FC<AlertBoxProps> = ({
     onClose,
     children,
 }) => {
+    const alertStyles: Record<string, string> = {
+        success: 'bg-green-100 border-green-500 text-green-700',
+        error: 'bg-red-100 border-red-500 text-red-700',
+        warning: 'bg-blue-100 border-blue-500 text-blue-700',
+        info: 'bg-blue-100 border-blue-500 text-blue-700',
+    };
+
   return (
     <div>
-        {/*      */}
+        {/* */}
     </div>
   );
 };
